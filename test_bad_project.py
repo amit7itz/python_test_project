1e53c0bbe7dd1c2568476c727c8428c6e26d2472
Amit Itzkovitch <amit7itz@gmail.com>
2023-10-20 12:06:02 +0300

    added files

---
+++ test_bad_project.py
@@ -0,0 +1,5 @@
+def function_with_flake_errors(number: int, second_number: int) -> None:
+    try:
+        number / second_number
+    except ZeroDivisionError:
+        print("You had an exception!")
